b44c86f3545f28533e6fa73b4b19fb5c0e565306
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-22 17:12:48 -0500

    not found 404

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const morgan = require('morgan');
 const taskRouter = require('./tasks/task.router.js');
+const {statusCodes, StatusCodes} = require('http-status-codes');
 const cors = require('cors');
 const responseFormatter = require('./middlewares/responseFormatter.js');
 
@@ -25,5 +26,7 @@ let accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log')
 app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use('/', taskRouter);
+
+app.use((req, res) => res.status(StatusCodes.NOT_FOUND).json(null))
 
 app.listen(port, () => console.log(`The app listening on port http://localhost:${port}`));
