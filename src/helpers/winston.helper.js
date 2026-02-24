51c62b4cca667a2423e26557ae34aefaf05199a5
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-24 15:54:36 -0500

    creating winston loger helper file

---
+++ src/helpers/winston.helper.js
@@ -0,0 +1,29 @@
+const winston = require('winston');
+const path = require('path');
+
+/**
+ error (0)
+ warn (1)
+ info (2)
+ verbose (3)
+ debug (4)
+ silly (5)
+ */
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        winston.format.printf((info) => `${info.timestamp} [${info.level}] : ${info.message}`))
+});
+
+const transports = [
+    new winston.transports.Console({level: 'info', format: winston.format.combine(winston.format.colorize())}),
+    new winston.transports.File({
+        filename: path.join(__dirname, '../../logs/info.log'), level: 'info', format: winston.format.json()
+    }),
+    new winston.transports.File({
+        filename: path.join(__dirname, '../../logs/error.log'), level: 'error', format: winston.format.json()
+    }),
+]
+
+module.exports = winston.createLogger({transports});
