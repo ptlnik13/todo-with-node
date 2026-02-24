e41dfb6db495fcb293d8092d8b253992ddf1b03a
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-24 10:16:14 -0500

    connecting mongoose to MongoDB database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -8,6 +9,7 @@ const cors = require('cors');
 const responseFormatter = require('./middlewares/responseFormatter.js');
 const authRouter = require("./auth/auth.router");
 const userRouter = require("./users/users.router");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -32,4 +34,16 @@ app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use((req, res) => res.status(StatusCodes.NOT_FOUND).json(null))
 
-app.listen(port, () => console.log(`The app listening on port http://localhost:${port}`));
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECTION, {dbName: 'fullstackTasks'})
+        console.log('Connected to MongoDB')
+        app.listen(port, () => console.log(`The app listening on port http://localhost:${port}`));
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+bootstrap();
