63950c0ca8afb9ff555d3c7b2a588c08fb106b86
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-24 16:04:36 -0500

    setting up winston middleware

--- src/helpers/winston.helper.js
+++ src/helpers/winston.helper.js
@@ -10,20 +10,22 @@ const path = require('path');
  silly (5)
  */
 
-const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        winston.format.printf((info) => `${info.timestamp} [${info.level}] : ${info.message}`))
-});
 
 const transports = [
     new winston.transports.Console({level: 'info', format: winston.format.combine(winston.format.colorize())}),
     new winston.transports.File({
-        filename: path.join(__dirname, '../../logs/info.log'), level: 'info', format: winston.format.json()
+        filename: path.join(__dirname, '..', '..', 'logs', 'info.log'), level: 'info', format: winston.format.json()
     }),
     new winston.transports.File({
-        filename: path.join(__dirname, '../../logs/error.log'), level: 'error', format: winston.format.json()
+        filename: path.join(__dirname, '..', '..', 'logs', 'error.log'), level: 'error', format: winston.format.json()
     }),
 ]
 
-module.exports = winston.createLogger({transports});
+const logger = winston.createLogger({
+    format    : winston.format.combine(
+        winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        winston.format.printf((info) => `${info.timestamp} [${info.level}] : ${info.message}`)),
+    transports: transports
+});
+
+module.exports = logger
