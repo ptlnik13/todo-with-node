4523508f0e2d067a6af766a29417f16ca13a1124
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-24 11:15:26 -0500

    delete task provider and controller updates

--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -2,6 +2,7 @@ const {StatusCodes} = require("http-status-codes");
 const createTaskProvider = require("./providers/createTask.provider");
 const getTasksProvider = require("./providers/getTasks.provider");
 const updateTaskProvider = require("./providers/updateTask.provider");
+const deleteTaskProvider = require("./providers/deleteTask.provider");
 
 async function handleGetTask(req, res) {
     let tasks = await getTasksProvider(req, res);
@@ -18,8 +19,9 @@ async function handlePatchTask(req, res) {
     res.status(StatusCodes.OK).json(updatedTask);
 }
 
-function handleDeleteTask(req, res) {
-    res.send('DELETE Task Controller');
+async function handleDeleteTask(req, res) {
+    const deletedTask = await deleteTaskProvider(req, res);
+    res.status(StatusCodes.OK).json(deletedTask);
 }
 
 module.exports = {handleGetTask, handlePostTask, handlePatchTask, handleDeleteTask};
