0003be9c77c2e5277f1ecc44430e047e769d6303
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-24 13:16:29 -0500

    getting started with express-validator

--- src/tasks/task.router.js
+++ src/tasks/task.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 const taskRouter = express.Router();
 
 const {
@@ -7,9 +8,22 @@ const {
     handlePatchTask,
     handleDeleteTask
 } = require("./tasks.controller");
+const {StatusCodes} = require("http-status-codes");
 
 taskRouter.get('/tasks', handleGetTask)
-taskRouter.post('/tasks', handlePostTask)
+taskRouter.post('/tasks', [
+        body('title', 'The Title can not be empty!').notEmpty(),
+        body('title', 'Title must be a string!').isString(),
+        body('dueDate', 'dueDate needs to be valid ISO8601 string').notEmpty().isISO8601(),
+    ],
+    (req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            return handlePostTask(req, res)
+        } else {
+            return res.status(StatusCodes.BAD_REQUEST).json(result.array())
+        }
+    })
 taskRouter.patch('/tasks', handlePatchTask)
 taskRouter.delete('/tasks', handleDeleteTask)
 
