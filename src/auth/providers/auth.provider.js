c96796fc9de2697a02fe58e142d4dbdeac53bab1
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-26 16:05:10 -0500

    comparing password to hash

--- src/auth/providers/auth.provider.js
+++ src/auth/providers/auth.provider.js
@@ -1,16 +1,25 @@
 const {matchedData} = require("express-validator");
 const errorLogger = require("../../helpers/errorLogger.helper");
 const {StatusCodes} = require("http-status-codes");
+const User = require("../../users/user.schema");
+const {compare} = require("bcrypt");
 
 async function authProvider(req, res) {
     const validateData = matchedData(req);
 
     try {
+        // get user from DB
+        const user = await User.findOne({email: validateData.email});
+        //compare hash
+        const result = await compare(validateData.password, user.password);
+        if (!result) {
+            return res.status(StatusCodes.BAD_REQUEST).json({reason: 'Invalid credentials'});
+        }
 
-        return res.status(StatusCodes.OK).json({});
+        return res.status(StatusCodes.OK).json({login: true});
     } catch (error) {
         errorLogger("Error while login:", req, error);
-        return res.status(StatusCodes.FORBIDDEN).json({reason: 'Failed to login'});
+        return res.status(StatusCodes.BAD_REQUEST).json({reason: 'Failed to login'});
     }
 }
 
