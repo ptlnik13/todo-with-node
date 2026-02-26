c3179b520aba2691fafe56e2995f71f25d3e4416
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-26 12:43:42 -0500

    adding task and user relation

--- src/tasks/validators/createTask.validator.js
+++ src/tasks/validators/createTask.validator.js
@@ -10,7 +10,8 @@ const createTaskValidator = [
     body('description', "The description can not be empty").notEmpty().isString().trim(),
     body('description', "The description must be between 3 and 500 characters").isLength({min: 3, max: 500}),
     body('priority').isIn(['low', 'normal', 'high']),
-    body('status').isIn(['todo', 'inProgress', 'completed']).trim()
+    body('status').isIn(['todo', 'inProgress', 'completed']).trim(),
+    body('user').notEmpty().isMongoId(),
 ];
 
 module.exports = createTaskValidator;
