989b4a3eb6da66eba5ed7de34e2599a929dcd56a
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-26 17:58:07 -0500

    removing hardcoded user ID and putting via JWT

--- src/tasks/validators/createTask.validator.js
+++ src/tasks/validators/createTask.validator.js
@@ -10,8 +10,7 @@ const createTaskValidator = [
     body('description', "The description can not be empty").notEmpty().isString().trim(),
     body('description', "The description must be between 3 and 500 characters").isLength({min: 3, max: 500}),
     body('priority').isIn(['low', 'normal', 'high']),
-    body('status').isIn(['todo', 'inProgress', 'completed']).trim(),
-    body('user').notEmpty().isMongoId(),
+    body('status').isIn(['todo', 'inProgress', 'completed']).trim()
 ];
 
 module.exports = createTaskValidator;
