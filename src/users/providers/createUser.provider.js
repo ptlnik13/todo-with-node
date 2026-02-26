0119ecc6410f20bf6d477b81b41f1b22dc3e8ad3
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-26 13:30:42 -0500

    bcrypt for hashing

--- src/users/providers/createUser.provider.js
+++ src/users/providers/createUser.provider.js
@@ -2,19 +2,21 @@ const User = require("../user.schema");
 const {matchedData} = require("express-validator");
 const {StatusCodes} = require("http-status-codes");
 const errorLogger = require("../../helpers/errorLogger.helper");
+const bcrypt = require("bcrypt");
 
 async function createUserProvider(req, res) {
 
     const validatedResult = matchedData(req);
     try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(validatedResult.password, salt);
         const {firstName, lastName, email, password} = validatedResult;
         const user = new User({
-            firstName, lastName, email, password
+            firstName, lastName, email, password: hashedPassword
         });
 
         await user.save()
-        delete user.password; // not returning password back to client, not required.
-        return res.status(StatusCodes.CREATED).json(user)
+        return res.status(StatusCodes.CREATED).json({_id: user._id, firstName, lastName, email})
     } catch (e) {
         errorLogger(`Error while creating user: ${e.message}`, req, e)
         return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
